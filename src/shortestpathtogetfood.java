fd5ff16ddce45c77be0badaa064544803384cbe9
DARSHAN R <darshanravichandran07@gmail.com>
2025-10-04 12:04:28 +0530

    Add initial project files and shortest path stub
    Added IntelliJ IDEA project configuration files, module file, and a new Java class 'shortestpathtogetfood' with a stub for the shortest path algorithm. The Java class includes the method signature and initial setup for grid traversal but is not yet fully implemented.

---
+++ src/shortestpathtogetfood.java
@@ -0,0 +1,32 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class shortestpathtogetfood {
+
+
+    public int getshortestPath(String[] grid) {
+
+
+        int m=grid.length;
+        int n=grid[0].length();
+
+        Queue<int[]> queue=new LinkedList<>();
+
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<m;j++){
+                if()
+            }
+        }
+
+
+
+
+
+        return 0;
+
+
+    }
+
+
+}
