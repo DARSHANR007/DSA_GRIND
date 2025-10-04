0d8a789af1292e18974b7150190605c7027e0054
DARSHAN R <darshanravichandran07@gmail.com>
2025-10-04 20:47:37 +0530

    Update NumberOfprovinces.java

--- src/NumberOfprovinces.java
+++ src/NumberOfprovinces.java
@@ -1,2 +1,25 @@
 public class NumberOfprovinces {
+
+    public int findCircleNum(int[][] isConnected) {
+
+
+        int m= isConnected.length;
+
+        int [] cities=new int[m];
+
+        int []size=new int[m];
+
+        for(int i=0;i<m;i++){
+            cities[i]=i;
+            size[i]=1;
+        }
+
+
+        return 0;
+
+    }
+
+    public void union( int a, int b){
+        
+    }
 }
