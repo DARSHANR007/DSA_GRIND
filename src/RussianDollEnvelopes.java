9f25a0277fb11fb3dae67160532aa70a475b3277
DARSHAN R <darshanravichandran07@gmail.com>
2025-10-08 22:34:25 +0530

    Update RussianDollEnvelopes.java

--- src/RussianDollEnvelopes.java
+++ src/RussianDollEnvelopes.java
@@ -1,30 +1,23 @@
+import java.util.Arrays;
+
 public class RussianDollEnvelopes {
 
     public int maxEnvelopes(int[][] envelopes) {
 
-        int count = 0;
+
+        int n=envelopes.length;
+
+        int [] dp= new int[n+1];
+
+        Arrays.sort();
 
 
-        int n = envelopes.length;
 
-        int[] larger_size = envelopes[0];
 
-        for (int i = 1; i < n; i++) {
 
-            int curr_height = envelopes[i][1];
-            int curr_weight = envelopes[i][0];
+        return 0;
 
-            if (curr_weight < larger_size[0] && curr_height < larger_size[1]) {
-                count++;
-            } else if (curr_weight > larger_size[0] && curr_height > larger_size[1]) {
-                count++;
-                larger_size[0] = curr_weight;
-                larger_size[1] = curr_height;
-            }
 
-        }
-
-        return count;
 
 
     }
