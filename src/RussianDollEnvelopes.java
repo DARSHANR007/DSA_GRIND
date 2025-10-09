a2b58402b66a94edb879b2fa9c99b532b9538260
DARSHAN R <darshanravichandran07@gmail.com>
2025-10-09 21:55:51 +0530

    Update RussianDollEnvelopes.java

--- src/RussianDollEnvelopes.java
+++ src/RussianDollEnvelopes.java
@@ -9,16 +9,59 @@ public class RussianDollEnvelopes {
 
         int [] dp= new int[n+1];
 
-        Arrays.sort();
+        int []heights=new int[n];
+
+        Arrays.fill(dp,1);
+
+        Arrays.sort(envelopes, (a, b) -> {
+            if (a[0] == b[0]) {
+                return b[1] - a[1]; // same width -> sort by height descending
+            } else {
+                return a[0] - b[0]; // otherwise sort by width ascending
+            }
+        });
+
+        for(int i=0;i<n;i++){
+            heights[i]=envelopes[i][1];
+        }
+
+        int len=0;
+
+        for(int num:heights){
+
+            int index = lowerBound(dp,n+1,num);
+
+            dp[index]=num;
+
+            if(index==len){
+                len++;
+            }
+
+        }
 
 
 
+        return len;
 
+    }
 
-        return 0;
+    public static int lowerBound(int[] dp, int size, int target){
 
+        int low = 0, high = size - 1;
+        while (low <= high){
 
+            int mid = low + (high - low) / 2;
 
+            if(dp[mid] <= target){
+                low=mid+1;
+            }
+            else{
+                high=mid;
+            }
+
+        }
+
+        return low;
 
     }
 
