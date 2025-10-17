d4b6187a60e0bf155908f7e81401b8f7867ff0e8
darsh <darshanravichandran07@gmail.com>
2025-10-17 21:42:15 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,51 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Main {
-
-    public static String solve(String str)
-    {
-        int n=str.length();
-
-        boolean[][] pal=new boolean[n][n];
-
-        for(int i=0;i<n;i++){
-            pal[i][i]=true;
-        }
-
-        for(int l=2;l<=n;l++){
-
-            for(int i=0;i<=n-l;i++){
-
-                int j=i+l-1;
-
-                if(str.charAt(i)==str.charAt(j)){
-                    pal[i][j]=(l==2)||pal[i+1][j-1];
-                }
-            }
-        }
-        int[] zero=new int[n+1];
-        int[] one=new int[n+1];
-
-        for(int i=0;i<n;i++){
-
-
-            zero[i+1]=zero[i]+(str.charAt(i)=='0'?1:0);
-
-            one[i+1]=one[i]+(str.charAt(i)=='1'?1:0);
-        }
-
-        for(int i=0;i<n;i++){
-            for(int j=i;j < n;j++){
-                if(pal[i][j]){
-
-                    int z=zero[i]+(zero[n] - zero[j+1]);
-                    int o=one[i]+(one[n]-one[j+1]);
-
-                    if(z<=o){
-
-                        StringBuilder res= new StringBuilder();
-
-                        for(int k=0;k<z;k++){res.append('0');
-                        }
-
-                        for(int k=0;k<o;k++){
-
-                            res.append('1');
-                        }
-                        return res.toString();
-                    }
-                }
-            }
-        }
-
-        return str;
-    }
-
+public class Main{
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int t=sc.nextInt();
+        Scanner s=new Scanner(System.in);
+        int t=s.nextInt();
+        while(t-->0)
+        {
+            long n=s.nextLong();
+            long m=s.nextLong();
 
-        while(t-->0){
-            String s=sc.next();
-            System.out.println(solve(s));
+            if(n==m){
+                System.out.println(0);
+                continue;
+            }
+
+            int i=0;
+
+            long change=n;
+
+            while((1L<<i)<=change)i++;
+            i--;
+
+            long full = 0;
+
+            for(int j=0;j<= i;j++) {
+                full|=(1L<<j);
+            }
+
+            if(m>full){
+                System.out.println(-1);
+            }
+            else{
+
+                long xorres=n^m;
+
+                if(xorres<=n)
+
+                {
+                    System.out.println(1);
+                    System.out.println(xorres);
+                }
+                else{
+                    System.out.println(2);
+                    System.out.println((full^n)+" "+(full^m));
+                }
+            }
         }
+
     }
 }
