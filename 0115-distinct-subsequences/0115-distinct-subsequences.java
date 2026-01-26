3e0db537a3e66886f0f25960c4aee9ca38142082
Darshan Ravichandran <darshanravichandran07@gmail.com>
2026-01-26 20:44:02 +0530

    Time: 14 ms (92.06%), Space: 43 MB (90.8%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -6,26 +6,41 @@ class Solution {
         int n=t.length();
 
 
-        int[][] dp= new int[m+1][n+1];
+        // int[][] dp= new int[m+1][n+1];
 
 
-        for(int i=0;i<=m;i++){
+        // for(int i=0;i<=m;i++){
 
-            dp[i][0]=1;  // if t is 0 , it can always be formed
+        //     dp[i][0]=1;  // if t is 0 , it can always be formed
 
-        }       
+        // }       
+
+
+        // for(int i=1;i<=m;i++){
+        //     for(int j=1;j<=n;j++){
+
+        //         dp[i][j]=dp[i-1][j];
+
+        //         if(s.charAt(i-1)==t.charAt(j-1)) dp[i][j]+=dp[i-1][j-1];
+            
+        //     }
+        // }
+
+        // return dp[m][n];
+
+
+        int[] dp= new int[n+1];
+
+        dp[0]=1;
 
 
         for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
+            for(int j=n;j>=1;j--){
 
-                dp[i][j]=dp[i-1][j];
-
-                if(s.charAt(i-1)==t.charAt(j-1)) dp[i][j]+=dp[i-1][j-1];
-            
+                if(s.charAt(i-1)==t.charAt(j-1)) dp[j]+=dp[j-1];
             }
         }
 
-        return dp[m][n];
+        return dp[n];
     }
 }
