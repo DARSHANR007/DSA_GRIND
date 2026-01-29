f204292759bffc505dfd77935e06f8d0cf61ac90
Darshan Ravichandran <darshanravichandran07@gmail.com>
2026-01-29 21:38:13 +0530

    Time: 28 ms (25.77%), Space: 43.2 MB (38.65%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -2,37 +2,33 @@ class Solution {
     public int maxPalindromes(String s, int k) {
 
         int n = s.length();
-
-        boolean[][] pal = new boolean[n][n];
-
-        for(int i=0;i<n;i++)
-            pal[i][i] = true;
-
-        for(int i=0;i<n-1;i++){
-            if(s.charAt(i)==s.charAt(i+1))
-                pal[i][i+1] = true;
-        }
-
-        for(int len=3; len<=n; len++){
-            for(int i=0; i<=n-len; i++){
-                int j = i+len-1;
-                if(s.charAt(i)==s.charAt(j) && pal[i+1][j-1])
-                    pal[i][j] = true;
-            }
-        }
-
         int[] dp = new int[n+1];
 
-        for(int i=1;i<=n;i++){
-            dp[i] = dp[i-1];
+        for(int center = 0; center < n; center++){
 
-            if(i-k >= 0 && pal[i-k][i-1])
-                dp[i] = Math.max(dp[i], dp[i-k] + 1);
-
-            if(i-k-1 >= 0 && pal[i-k-1][i-1])
-                dp[i] = Math.max(dp[i], dp[i-k-1] + 1);
+            expand(s, center, center, k, dp);     // odd
+            expand(s, center, center+1, k, dp);   // even
         }
 
         return dp[n];
+    }
+
+    private void expand(String s, int l, int r, int k, int[] dp){
+
+        int n = s.length();
+
+        while(l >= 0 && r < n && s.charAt(l) == s.charAt(r)){
+
+            int len = r - l + 1;
+
+            if(len >= k){
+                dp[r+1] = Math.max(dp[r+1], dp[l] + 1);
+            }
+
+            dp[r+1] = Math.max(dp[r+1], dp[r]); 
+            
+            l--;
+            r++;
+        }
     }
 }
