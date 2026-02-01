9f8dfa574872dcfd26626579c4a830c67eb5ff64
Darshan Ravichandran <darshanravichandran07@gmail.com>
2026-02-01 12:27:15 +0530

    Time: 0 ms (100%), Space: 43 MB (22.94%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.java
@@ -1,15 +1,23 @@
 class Solution {
     public char repeatedCharacter(String s) {
 
-        int[] arr = new int[26];
+        int mask=0;
+
+        
 
         for (char x : s.toCharArray()) {
 
-            if (arr[x - 'a'] == 0) {
-                arr[x - 'a']++;
-            } else {
-                return x;
-            }
+            int bit=1 << (x-'a');
+
+
+            if( (bit & mask) !=0) return x;
+
+
+            mask|=bit;
+
+
+
+           
         }
 
         return ' '; 
