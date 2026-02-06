5f64ad0525742bec1ed88203b416eaacf70d7f95
Darshan Ravichandran <darshanravichandran07@gmail.com>
2026-02-06 10:40:05 +0530

    Time: 25 ms (58.63%), Space: 86.1 MB (60.24%) - LeetHub

--- 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.java
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.java
@@ -1,48 +1,16 @@
-import java.util.Arrays;
-
 class Solution {
     public int minRemoval(int[] nums, int k) {
-        int n = nums.length;
-        if (n <= 1) return 0;
-
         Arrays.sort(nums);
+        int i = 0;
+        int maxLen = 0;
 
-        int mini = n; 
-
-        for (int left = 0; left < n; left++) {
-            long tofind = (long) nums[left] * k;
-            int check = upperbound(nums, tofind);
-
-            int lastValidIdx;
-            if (check == -1) {
-                lastValidIdx = n;
-            } else {
-                lastValidIdx = check;
+        for (int j = 0; j < nums.length; j++) {
+            while ((long) nums[j] > (long) nums[i] * k) {
+                i++;
             }
-
-            int kept = lastValidIdx - left;
-            int removing = n - kept;
-
-            mini = Math.min(mini, removing);
+            maxLen = Math.max(maxLen, j - i + 1);
         }
 
-        return mini;
-    }
-
-    public int upperbound(int[] nums, long k) {
-        int answer = -1;
-        int left = 0;
-        int right = nums.length - 1;
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] > k) {
-                answer = mid;
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return answer; 
+        return nums.length - maxLen;
     }
 }
