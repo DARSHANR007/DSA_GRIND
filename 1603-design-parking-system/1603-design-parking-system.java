5370706d0a9802562abf640610580cc24c634932
Darshan Ravichandran <darshanravichandran07@gmail.com>
2026-02-17 09:57:50 +0530

    Time: 7 ms (99.9%), Space: 47 MB (84.24%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.java
+++ 1603-design-parking-system/1603-design-parking-system.java
@@ -1,17 +1,23 @@
 class ParkingSystem {
-
-    int[] slots;
+    int big,medium,small;
 
     public ParkingSystem(int big, int medium, int small) {
-        slots = new int[4];  // index 1,2,3
-        slots[1] = big;
-        slots[2] = medium;
-        slots[3] = small;
+        this.big=big;
+        this.medium=medium;
+        this.small=small;
     }
     
     public boolean addCar(int carType) {
-        if (slots[carType] > 0) {
-            slots[carType]--;
+        if(carType==1 && this.big>=1){
+            this.big--;
+            return true;
+        }
+        if(carType==2 && this.medium>=1){
+            this.medium--;
+            return true;
+        }
+        if(carType==3 && this.small>=1){
+            this.small--;
             return true;
         }
         return false;
