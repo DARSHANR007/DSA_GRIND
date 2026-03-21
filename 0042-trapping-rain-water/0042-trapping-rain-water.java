0a01d00f90c393cf7e51c4654cf442ff9037a4d4
Darshan Ravichandran <darshanravichandran07@gmail.com>
2026-03-21 11:43:32 +0530

    Time: 1 ms (59.49%), Space: 48.8 MB (14.44%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,49 +1,26 @@
 class Solution {
     public int trap(int[] height) {
         int n = height.length;
-        if (n <= 2) return 0;
+        if (n == 0) return 0;
 
-        // 1. Build the Prefix Sum array (as you did)
-        int[] prefix = new int[n];
-        int currentSum = 0;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
+
+        leftMax[0] = height[0];
+        rightMax[n - 1] = height[n - 1];
+
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+
+            int j = n - 1 - i;
+            rightMax[j] = Math.max(rightMax[j + 1], height[j]);
+        }
+
+        int cap = 0;
         for (int i = 0; i < n; i++) {
-            currentSum += height[i];
-            prefix[i] = currentSum;
+            cap += Math.min(leftMax[i], rightMax[i]) - height[i];
         }
 
-        int totalWater = 0;
-        int left = 0;
-
-        // 2. Forward Pass: Find containers where right wall >= left wall
-        for (int right = 1; right < n; right++) {
-            if (height[right] >= height[left]) {
-                if (right - left > 1) {
-                    // Formula: (width * height of shorter wall) - sum of bars in between
-                    int width = right - left - 1;
-                    int fillHeight = height[left];
-                    int barsInBetween = prefix[right - 1] - prefix[left];
-                    totalWater += (width * fillHeight) - barsInBetween;
-                }
-                left = right;
-            }
-        }
-
-        // 3. Backward Pass: Handle the remaining part (where left wall was tallest)
-        int tallestSoFar = left; // This was the last "left" we reached
-        int right = n - 1;
-        for (int i = n - 2; i >= tallestSoFar; i--) {
-            if (height[i] >= height[right]) {
-                if (right - i > 1) {
-                    int width = right - i - 1;
-                    int fillHeight = height[right];
-                    // Using prefix sum to get bars between i and right
-                    int barsInBetween = prefix[right - 1] - prefix[i];
-                    totalWater += (width * fillHeight) - barsInBetween;
-                }
-                right = i;
-            }
-        }
-
-        return totalWater;
+        return cap;
     }
 }
