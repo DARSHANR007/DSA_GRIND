fce7611078ccbd1cf4dcc943c6b899ef9dbc6e3c
Darshan Ravichandran <darshanravichandran07@gmail.com>
2026-04-15 19:30:20 +0530

    Time: 53 ms (16.8%), Space: 50.4 MB (16.57%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.java
@@ -1,29 +1,50 @@
 class Solution {
-
     public int subarraysWithKDistinct(int[] nums, int k) {
-        return helper(nums, k) - helper(nums, k - 1);
+
+
+        return helper(nums,k)-helper(nums,k-1);
+
+        
     }
 
-    public int helper(int[] nums, int k) {
-        int left = 0, count = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
 
-        for (int right = 0; right < nums.length; right++) {
-            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+    public int helper(int[] nums, int k){
 
-            while (map.size() > k) {
-                map.put(nums[left], map.get(nums[left]) - 1);
 
-                if (map.get(nums[left]) == 0) {
-                    map.remove(nums[left]);
+        HashMap<Integer,Integer> hash= new HashMap();
+
+
+        int left=0;
+
+        int n=nums.length;
+
+
+        int count=0;
+
+        for(int right=0;right<n;right++){
+
+
+            hash.put(nums[right],hash.getOrDefault(nums[right],0)+1);
+
+
+            while(hash.size()>k){
+
+                hash.put( nums[left],hash.get(nums[left])-1);
+
+                if(hash.get(nums[left])==0){
+                    hash.remove(nums[left]);
                 }
 
                 left++;
             }
 
-            count += right - left + 1;
+
+            count+=right-left+1;
         }
 
         return count;
+
+
+
     }
 }
